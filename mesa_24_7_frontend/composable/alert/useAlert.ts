a2c1d5982994cc7585a156ae143faacf5cbd4ede
Piero Alvaro Salazar Calle <54618161+Pieromental@users.noreply.github.com>
2025-04-21 18:26:19 -0500

    [#0026] Add Alert Composable

---
+++ mesa_24_7_frontend/composable/alert/useAlert.ts
@@ -0,0 +1,162 @@
+import { useQuasar } from 'quasar';
+
+
+export function useAlert() {
+  const $q = useQuasar();
+
+  /****************************************************************************/
+  /*                             METHODS                                      */
+  /****************************************************************************/
+  interface typeAlert {
+    type: 'success' | 'error' | 'question' | 'information' | 'warning';
+  }
+
+  const arrImg = [
+    { src: '/images/icons/alert/positive.png', type: 'success' },
+    { src: '/images/icons/alert/negative.png', type: 'error' },
+    { src: '/images/icons/alert/question.png', type: 'question' },
+    { src: '/images/icons/alert/information.png', type: 'information' },
+    { src: '/images/icons/alert/warning.png', type: 'warning' },
+  ];
+
+  const singleAlert = async (
+    tipoStr: typeAlert,
+    titleStr: string,
+    messageStr: string
+  ) => {
+    const pos = arrImg.findIndex((obj) => obj.type === tipoStr.type);
+    return new Promise((resolve) => {
+      $q.dialog({
+        title: '',
+        message:
+          `<div class="row">
+          <div class="col-12 text-center">
+            <h5 style="margin:5px;">` +
+          titleStr +
+          `</h5>
+          </div>
+          <div class="col-12 text-center">
+            <img width="100" style="margin:5px;" src=` +
+          arrImg[pos].src +
+          `>
+          </div>
+          <div class="col-12 text-center">
+            <p style="margin:5px;">` +
+          messageStr +
+          `</p>
+          </div>
+        </div>`,
+        html: true,
+        ok: {
+          label: 'Aceptar',
+          color: 'primary',
+          flat: true,
+        },
+        persistent: true,
+      }).onOk(() => {
+        resolve(true);
+      });
+    });
+  };
+
+  const confirmAlert = async (
+    tipoStr: typeAlert,
+    titleStr: string,
+    messageStr: string,
+    okLabel?: string,
+    cancelLabel?: string
+  ) => {
+    const pos = arrImg.findIndex((obj) => obj.type === tipoStr.type);
+    return new Promise((resolve) => {
+      $q.dialog({
+        title: '',
+        message:
+          `<div class="row">
+          <div class="col-12 text-center">
+            <h5 style="margin:5px;">` +
+          titleStr +
+          `</h5>
+          </div>
+          <div class="col-12 text-center">
+            <img width="100" style="margin:5px;" src=` +
+          arrImg[pos].src +
+          `>
+          </div>
+          <div class="col-12 text-center">
+            <p style="margin:5px;">` +
+          messageStr +
+          `</p>
+          </div>
+        </div>`,
+        html: true,
+        ok: {
+          label: okLabel || 'Aceptar',
+          color: 'primary',
+          flat: true,
+        },
+        cancel: {
+          label: cancelLabel || 'Cancelar',
+          color: 'negative',
+          flat: true,
+        },
+        persistent: true,
+      })
+        .onOk(() => {
+          resolve(true);
+        })
+        .onCancel(() => {
+          resolve(false);
+        });
+    });
+  };
+  const promptAlert = async (
+    tipoStr: typeAlert,
+    titleStr: string,
+    messageStr: string,
+    defaultValue: string 
+  ) => {
+    return new Promise((resolve) => {
+      $q.dialog({
+        title: '',
+        message:
+          `<div class="row">
+              <div class="col-12 text-center">
+                <h5 style="margin:5px;">` +
+          titleStr +
+          `</h5>
+              </div>
+              <div class="col-12 text-center">
+            <p style="margin:5px;">` +
+          messageStr +
+          `</p>
+          </div>
+            </div>`,
+        html: true,
+        prompt: {
+          model: defaultValue,
+          isValid: (val) => val.length > 0, 
+          type: 'text', 
+        },
+        ok: {
+          label: 'Aceptar',
+          color: 'primary',
+          flat: true,
+        },
+        cancel: {
+          label: 'Cancelar',
+          color: 'negative',
+          flat: true,
+        },
+        persistent: true,
+      })
+        .onOk((data) => {
+          resolve(data); 
+        })
+        .onCancel(() => {
+          resolve(null);
+        });
+    });
+  };
+
+  return { singleAlert, confirmAlert, promptAlert };
+}
