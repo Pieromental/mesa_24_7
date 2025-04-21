7ec84eb6f1174963d3735bfefc06fdcf5eef6239
Piero Alvaro Salazar Calle <54618161+Pieromental@users.noreply.github.com>
2025-04-21 18:32:21 -0500

    [#0031] Add Dashboard Component

---
+++ mesa_24_7_frontend/src/components/DashboardComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="q-pa-xl flex flex-center column">
+    <div class="q-mb-md full-width q-pa-sm flex flex-center">
+      <img
+        class="welcome-image"
+        src="~assets/images/svg/mesa24_dashboard.svg"
+        alt="Mesa 24/7 dashboard"
+      />
+    </div>
+    <div class="text-h4 text-bold q-mt-md text-center">¡Bienvenido a Mesa 24/7!</div>
+    <div class="text-subtitle2 text-grey text-center">
+      Aquí podrás gestionar comensales, mesas y reservas.
+    </div>
+    
+  </q-page>
+</template>
+
+<style scoped>
+.welcome-image {
+  max-width: 100%;
+  width: 500px;
+  height: auto;
+}
+</style>
